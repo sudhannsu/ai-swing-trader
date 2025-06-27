224ab14a4e4bbd36677f171766ff213de86c3f3c
sudhannsu <41715197+sudhannsu@users.noreply.github.com>
2025-06-27 11:11:14 +0530

    Update nifty_symbols.py

--- utils/nifty_symbols.py
+++ utils/nifty_symbols.py
@@ -9,10 +9,7 @@ def fetch_top_nifty_symbols(limit=50):
     nse = Nse()
     codes = nse.get_stock_codes()  # dict: {SYMBOL: Company Name}
     print("Fetched codes:", codes)
-    # Remove the first key which is 'SYMBOL'
-    # stock_list = list(codes.keys())[1:]
-    stock_list = codes
-    
+    stock_list = codes    
     # Append '.NS' to match yfinance format
     stock_list = [symbol + '.NS' for symbol in stock_list[:limit]]
 
